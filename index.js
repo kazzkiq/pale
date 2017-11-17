79f2ec3af57e5d829b7d5480aac1090db8d84f7c
Claudio <claudiohw@gmail.com>
2017-11-17 10:40:10 -0200

    Fixing UMD

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports', 'pale'], factory);
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-        // CommonJS
-        factory(exports, require('pale'));
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
-        // Browser globals
-        factory((root.commonJsStrict = {}), root.pale);
-    }
-}(typeof self !== 'undefined' ? self : this, function (exports, pale) {
-    // Use b in some fashion.
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
 
-    // attach properties to the exports object to define
-    // the exported module properties.
-    exports.action = function() {
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return function() {
   /**
    * Represents pure functions that can validate an entry.
    * Those functions always return a boolean
@@ -168,5 +170,5 @@
       return validators_list;
     }
   }
-}
+};
 }));
